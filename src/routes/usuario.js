bbb15b3dd1c11255f9a6620ec03a761e5c7b4bdb
EliasVasques <eliasvasques85@gmail.com>
2022-09-22 22:03:58 -0300

    adição: edição funcionando

--- src/routes/usuario.js
+++ src/routes/usuario.js
@@ -4,6 +4,13 @@ const { ObjectId } = require('mongodb')
 
 const router = Router()
 
+const usuarioDefault = {
+    nome: '',
+    email: '',
+    sexo: '',
+    status: '',
+    _id: ''
+}
 // sudo systemctl start mongod
 
 router.get('/', (req, res)  => {
@@ -13,17 +20,17 @@ router.get('/', (req, res)  => {
 })
 
 router.get('/add', (req, res) => {
-    res.render('add-usuarios.ejs', { msg: "" })
+    res.render('add-usuarios.ejs', { msg: "",  usuario: usuarioDefault  })
 })
 
 router.post('/add', (req, res)  => {
     const resposta = new UsuarioSchema(req.body)
     console.log(req.body)
     resposta.save()
-        .then(usuario => res.render('add-usuarios.ejs', { msg: `${usuario.nome} adcionado com sucesso!`}))
+        .then(usuario => res.render('add-usuarios.ejs', { msg: `${usuario.nome} adcionado com sucesso!`, usuario: usuarioDefault }))
         .catch(err => {
             console.log(err)
-            res.render('add-usuarios.ejs', { msg: `Erro ao adcionar usuario!`})
+            res.render('add-usuarios.ejs', { msg: `Erro ao adcionar usuario!`, usuario: usuarioDefault  })
         })
 })
 
@@ -33,12 +40,24 @@ router.get('/editar/:id',(req, res) => {
             .then(usuario => res.render('editar-usuarios.ejs', { msg: "", usuario: usuario }))
             .catch(err => {
                 console.log(err)
-                res.render('editar-usuarios.ejs', { msg: "Usuário não encontrado!" })
+                res.render('editar-usuarios.ejs', { msg: "Usuário não encontrado!", usuario: usuarioDefault })
             })
     } else {
-        res.render('editar-usuarios.ejs', { msg: "Id inválido!" })
+        res.render('editar-usuarios.ejs', { msg: "Id inválido!", usuario: usuarioDefault  } )
     }
-    
+})
+
+router.post('/editar/:id',(req, res) => {
+    if(ObjectId.isValid(req.params.id)) {
+        UsuarioSchema.findByIdAndUpdate({ _id: ObjectId(req.params.id) }, req.body)
+            .then(() => res.render('editar-usuarios.ejs', { msg: "Atualizado com sucesso!", usuario: req.body }))
+            .catch(err => {
+                console.log(err)
+                res.render('editar-usuarios.ejs', { msg: "Usuário não encontrado!", usuario: usuarioDefault})
+            })
+    } else {
+        res.render('editar-usuarios.ejs', { msg: "Id inválido!", usuario: usuarioDefault  })
+    }
 })
 
 
