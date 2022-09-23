985ff4f565182363538c221317d9983041312678
EliasVasques <eliasvasques85@gmail.com>
2022-09-22 21:28:45 -0300

    adição: mesmo form tanto para editar quanto para adcionar, usando partials

--- src/app.js
+++ src/app.js
@@ -12,8 +12,6 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 
-
-
 app.set('view engine', 'ejs')
 
 app.use(express.json()) 
--- src/routes/usuario.js
+++ src/routes/usuario.js
@@ -1,14 +1,15 @@
 const { Router } = require('express')
 const UsuarioSchema = require('../schemas/Usuario')
+const { ObjectId } = require('mongodb')
 
 const router = Router()
 
+// sudo systemctl start mongod
 
 router.get('/', (req, res)  => {
     UsuarioSchema.find()
         .then(usuarios => res.render('lista-usuarios', { usuarios: usuarios }))
         .catch(err => console.log(err))
-     
 })
 
 router.get('/add', (req, res) => {
@@ -25,5 +26,20 @@ router.post('/add', (req, res)  => {
             res.render('add-usuarios.ejs', { msg: `Erro ao adcionar usuario!`})
         })
 })
+
+router.get('/editar/:id',(req, res) => {
+    if(ObjectId.isValid(req.params.id)) {
+        UsuarioSchema.findOne({ _id: ObjectId(req.params.id) })
+            .then(usuario => res.render('editar-usuarios.ejs', { msg: "", usuario: usuario }))
+            .catch(err => {
+                console.log(err)
+                res.render('editar-usuarios.ejs', { msg: "Usuário não encontrado!" })
+            })
+    } else {
+        res.render('editar-usuarios.ejs', { msg: "Id inválido!" })
+    }
+    
+})
+
 
 module.exports  = router
